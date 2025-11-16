a8288769e3f98d47a9db471c615e7047701a1769
djgomezsanjuan <djgomezsanjuan@gmail.com>
2025-11-16 17:48:29 -0500

    Enemy script completed

--- Assets/Scripts/scrEnemyScript.cs
+++ Assets/Scripts/scrEnemyScript.cs
@@ -1,46 +1,145 @@
-using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class scrEnemyScript : MonoBehaviour
 {
-    public int enemyHealth;
-    public int enemySpeed;
-    public int enemyDamage;
+    #region Public Variables
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance; //Minimum distance
+    public float moveSpeed;
+    public float timer; //Cooldown of attacks
+    #endregion
 
-    public GameObject enemyGorilla, enemySnake, enemyCrocodile, enemyPoacher;
+    #region Private Variables
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator animator;
+    private float distance; //store distance between enemy and player
+    private bool attackMode;
+    private bool inRange; //See if player in range
+    private bool cooling; //check if enemy is cooling after attack
+    private float intTimer;
+    #endregion 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        intTimer = timer;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        switch (other.tag)
+        if (inRange)
         {
-            case "Gorilla":
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            RaycastDebugger();
+        }
 
-                break;
+        //When player is detected
+        if (hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if (hit.collider == null)
+        {
+            inRange = false;
+        }
 
-            case "Snake":
-
-                break;
-
-            case "Crocodile":
-
-                break;
-
-            case "Poacher":
-
-                break;
+        if (inRange == false)
+        {
+            animator.SetBool("Run", false);
+            StopAttack();
         }
     }
 
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if (attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            Cooldown();
+            animator.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        animator.SetBool("Run", true);
+
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("enemy1Attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer;
+        attackMode = true;
+
+        animator.SetBool("Run", false);
+        animator.SetBool("Attack", true);
+    }
+
+    void Cooldown()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 && cooling && attackMode)
+        {
+            cooling = false;
+            timer = intTimer;
+        }
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        animator.SetBool("Attack", false);
+    }
+
+    void RaycastDebugger()
+    {
+        if (distance > attackDistance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        }
+        else if (attackDistance > distance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+        }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
 }
